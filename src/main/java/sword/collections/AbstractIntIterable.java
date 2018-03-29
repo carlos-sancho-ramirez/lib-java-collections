b482596f3782f4ea2f9dfffc29937f110a115ed6
Carlos Sancho Ramirez <carlos.sancho.ramirez@gmail.com>
2018-03-29 15:30:02 +0200

    Include toString to AbstractIntIterable for debugging reasons

--- src/main/java/sword/collections/AbstractIntIterable.java
+++ src/main/java/sword/collections/AbstractIntIterable.java
@@ -17,4 +17,21 @@ abstract class AbstractIntIterable extends AbstractSizable implements IterableIn
     public int hashCode() {
         return size();
     }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder(getClass().getSimpleName()).append('(');
+        boolean itemAdded = false;
+
+        for (int value : this) {
+            if (itemAdded) {
+                sb.append(',');
+            }
+
+            sb.append(value);
+            itemAdded = true;
+        }
+
+        return sb.append(')').toString();
+    }
 }
