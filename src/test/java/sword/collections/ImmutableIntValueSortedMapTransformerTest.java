b42faeca6a9386d318e101965644d06e991353be
Carlos Sancho Ramirez <carlos.sancho.ramirez@gmail.com>
2019-02-12 18:17:33 +0100

    Include ImmutableIntValueSortedMapTransformerTest

---
+++ src/test/java/sword/collections/ImmutableIntValueSortedMapTransformerTest.java
@@ -0,0 +1,53 @@
+package sword.collections;
+
+public final class ImmutableIntValueSortedMapTransformerTest extends IntTransformerTest<ImmutableIntValueSortedMap, IntCollectionBuilder<ImmutableIntValueSortedMap>> {
+
+    @Override
+    void withBuilder(Procedure<IntCollectionBuilder<ImmutableIntValueSortedMap>> procedure) {
+        procedure.apply(new SameKeyAndValueBuilder());
+        procedure.apply(new IndexedKeyBuilder());
+    }
+
+    @Override
+    void withMapToIntFunc(Procedure<IntToIntFunction> procedure) {
+        procedure.apply(v -> v);
+        procedure.apply(v -> v * v);
+        procedure.apply(v -> -v - 1);
+    }
+
+    @Override
+    void withMapFunc(Procedure<IntFunction<Object>> procedure) {
+        procedure.apply(Integer::toString);
+    }
+
+    private static final class SameKeyAndValueBuilder implements IntCollectionBuilder<ImmutableIntValueSortedMap> {
+        private final ImmutableIntValueSortedMap.Builder<String> builder = new ImmutableIntValueSortedMap.Builder<>(SortUtils::compareCharSequenceByUnicode);
+
+        @Override
+        public SameKeyAndValueBuilder add(int element) {
+            builder.put(Integer.toString(element), element);
+            return this;
+        }
+
+        @Override
+        public ImmutableIntValueSortedMap build() {
+            return builder.build();
+        }
+    }
+
+    private static final class IndexedKeyBuilder implements IntCollectionBuilder<ImmutableIntValueSortedMap> {
+        private final ImmutableIntValueSortedMap.Builder<Integer> builder = new ImmutableIntValueSortedMap.Builder<>((a, b) -> a > b);
+        private int key;
+
+        @Override
+        public IndexedKeyBuilder add(int element) {
+            builder.put(key++, element);
+            return this;
+        }
+
+        @Override
+        public ImmutableIntValueSortedMap build() {
+            return builder.build();
+        }
+    }
+}
